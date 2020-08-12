4a13c4989e950d52e186cf1827524ce946b2bc25
xuebiao <xuebiao@beisen.com>
2020-08-12 20:56:57 +0800

    hash相关

---
+++ LeetCode/LeetCode/Hash相关/TwoNumbersAdd.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    /// <summary>
+    /// 两数之和
+    /// </summary>
+    public static class TwoNumbersAdd
+    {
+
+        /// <summary>
+        /// 暴力破解O(n)
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public static int[] Violent(int[] nums,int target) 
+        {
+            int[] num = new int[2] { -1,-1};
+            for (int i=0; i<nums.Length; i++) 
+            {
+                for(int j = i+1; j < nums.Length; j++)
+                {
+                    if(nums[i]+nums[j]==target)
+                    {
+                        num[0] = i;
+                        num[1] = j;
+                        return num;
+                    }
+                }
+            }
+            return num;
+        }
+
+        /// <summary>
+        /// hash O(n)
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public static int[] HashCal(int[] nums, int target)
+        {
+            var dic = new Dictionary<int, int>();
+            for(int i = 0; i < nums.Length; i++)
+            {
+                if (dic.ContainsKey(target - nums[i]))
+                {
+                    return new int[] { dic[target - nums[i]], i };
+                }
+                dic[nums[i]] = i;
+            }
+            return new int[] { -1 };
+        }
+
+        
+    }
+}
--- LeetCode/LeetCode/Program.cs
+++ LeetCode/LeetCode/Program.cs
@@ -10,6 +10,13 @@ namespace LeetCode
     {
         static void Main(string[] args)
         {
+
+            var arr = new int[] { 1, 5, 2, 7, 4, 8, 3 };
+
+            //var res = TwoNumbersAdd.Violent(arr,16);
+            var res = TwoNumbersAdd.HashCal(arr, 7);
+            Console.WriteLine($"计算结果是：{res[0]} & {res[1]}");
+            Console.ReadLine();
         }
     }
 }
